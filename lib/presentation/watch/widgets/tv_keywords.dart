a5c80a3a16cf40e3283bd1c62a65700f64b6c07c
myunus <yunuslon97@gmail.com>
2026-03-01 00:19:10 +0700

    feat: update keyword toggle

--- lib/presentation/watch/widgets/tv_keywords.dart
+++ lib/presentation/watch/widgets/tv_keywords.dart
@@ -6,15 +6,22 @@ import 'package:movie/core/entity/keyword.dart';
 import 'package:movie/domain/tv/usecases/get_keyword.dart';
 import 'package:movie/service_locator.dart';
 
-class TvKeywords extends StatelessWidget {
+class TvKeywords extends StatefulWidget {
   final int tvId;
   const TvKeywords({super.key, required this.tvId});
 
   @override
+  State<TvKeywords> createState() => _TvKeywordsState();
+}
+
+class _TvKeywordsState extends State<TvKeywords> {
+  bool isExpanded = false;
+  @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
-          GenericDataCubit()..getData(sl<GetTVKeywordsUseCase>(), params: tvId),
+          GenericDataCubit()
+            ..getData(sl<GetTVKeywordsUseCase>(), params: widget.tvId),
       child: BlocBuilder<GenericDataCubit, GenericDataState>(
         builder: (context, state) {
           if (state is DataLoading) {
@@ -27,11 +34,39 @@ class TvKeywords extends StatelessWidget {
           }
           if (state is DataLoaded) {
             List<KeywordEntity> keyword = state.data;
+
+            final visibleCount = 5;
+            final shouldCollapse = keyword.length > visibleCount && !isExpanded;
+
+            final displayedItems = shouldCollapse
+                ? keyword.take(visibleCount).toList()
+                : keyword;
+
             return Wrap(
               spacing: 5,
-              children: keyword
-                  .map((item) => Chip(label: Text(item.name!)))
-                  .toList(),
+              children: [
+                ...displayedItems.map((item) => Chip(label: Text(item.name!))),
+
+                if (shouldCollapse)
+                  ActionChip(
+                    label: Text('+${keyword.length - visibleCount}'),
+                    onPressed: () {
+                      setState(() {
+                        isExpanded = true;
+                      });
+                    },
+                  ),
+
+                if (isExpanded && keyword.length > visibleCount)
+                  ActionChip(
+                    label: const Text('Show Less'),
+                    onPressed: () {
+                      setState(() {
+                        isExpanded = false;
+                      });
+                    },
+                  ),
+              ],
             );
           }
           if (state is FailureLoadData) {
